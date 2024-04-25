7de30be82f19a691ccc779fe59a56301695b11cf
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-04-25 15:54:06 -0300

    loading-component

--- src/components/Products/products.jsx
+++ src/components/Products/products.jsx
@@ -2,26 +2,34 @@ import React, { useState, useEffect } from "react"
 import './products.css'
 import fetchProducts from "../../api/fetchProducts"
 import ProductCard from "../ProductCard/productCard"
+import Loading from "../Loading/loading"
 
-export default function Products(){
+export default function Products() {
   const [products, setProducts] = useState([])
+
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
 
     fetchProducts('iphone').then((response) => { // fazendo requisição da api, pelo produto digitado na função entre parentese ('iphone'), e emitindo uma resposta = response por meio do .then() utilizando setProducts(response) 
 
       setProducts(response)
+      setLoading(false)
     });
 
   }, [])
 
 
-  return(
-    <section className="products container">
-      {
-        products.map((product) => <ProductCard key={product.id} data={product} />)
-      }
-    </section>
+  return (
+
+    (loading && <Loading />) || (
+      <section className="products container">
+        {
+          products.map((product) => <ProductCard key={product.id} data={product} />)
+        }
+      </section>
+    )
+
   )
 }
 
